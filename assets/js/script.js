6bc009e2e6c87aac2d498c9c9a661ddf741e4f20
nelsondcds <pipoca012008@yahoo.com>
2021-10-22 16:40:32 -0400

    added webpack

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
-$(document).ready(function() {
+require("bootstrap");
+$(function() {
   function createEl(htmlString, attrs, ...children) {
     if (typeof htmlString !== "string") {
       throw Error("Argument 'htmlString' is required and must be a string");
